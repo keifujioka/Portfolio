993235c91f527faf9ee8e2592f37da0d10e16d92
keifujioka <zro.20402525@gmail.com>
2026-08-22 03:22:13 +0900

    BallRolling
    3Dボールゲーム

---
+++ BallRolling/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-2026_07_18_ BallRolling-0.1.0";
+const contentToCache = [
+    "Build/BallRolling.loader.js",
+    "Build/BallRolling.framework.js",
+    "Build/BallRolling.data",
+    "Build/BallRolling.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
